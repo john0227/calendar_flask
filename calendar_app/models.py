a62a39e98baa9a7ea607e783b5ec9e8cbbc93a9e
명신민 <minjohn0227@gmail.com>
2023-12-02 23:34:56 +0900

    Support adding new schedule to DB

--- calendar_app/models.py
+++ calendar_app/models.py
@@ -1,15 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from . import app
+
+import os
+
+abs_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{abs_path}/db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class Schedule(db.Model):
-    sch_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    sch_name = db.Column(db.String(255), nullable=False)
-    sch_date = db.Column(db.Date, nullable=False)
-    start_time = db.Column(db.Time)
-    end_time = db.Column(db.Time)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.Integer, nullable=False)  # YYYYMMDD (8 digit integer)
+    start_time = db.Column(db.Integer)  # HHMM (4 digit integer, 24 hour format)
+    end_time = db.Column(db.Integer)  # HHMM (4 digit integer, 24 hour format)
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    task_name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255))
     done = db.Column(db.Boolean)
+
+with app.app_context():
+    db.create_all()
--- calendar_app/views.py
+++ calendar_app/views.py
@@ -2,6 +2,7 @@ from flask import redirect, render_template, request, url_for
 
 from . import app
 from .calendar_util import my_calendar
+from .models import db, Schedule, Task
 
 import datetime
 
@@ -14,5 +15,13 @@ def index():
 
 @app.post('/add_schedule')
 def add_schedule():
-    print(request.form)
+    # ImmutableMultiDict([('date', '2023-12-14'), ('schedule_name', 'adfasdf'), ('sch_start', '23:59'), ('sch_end', '23:59')])
+    new_sch = Schedule(
+        name=request.form['schedule_name'],
+        date=int(request.form['date'].replace('-', '')),
+        start_time=int(request.form['sch_start'].replace(':', '')),
+        end_time=int(request.form['sch_end'].replace(':', ''))
+    )
+    db.session.add(new_sch)
+    db.session.commit()
     return redirect(url_for('index'))
