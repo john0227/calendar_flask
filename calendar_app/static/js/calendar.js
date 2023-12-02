2fb21adb94e16cf86fdaa8adde9b45aeae57bbc1
명신민 <minjohn0227@gmail.com>
2023-12-02 23:15:01 +0900

    Add time field to add schedule form

--- calendar_app/static/js/calendar.js
+++ calendar_app/static/js/calendar.js
@@ -1,12 +1,20 @@
+function zeroPad2(num) {
+    return ('00' + num).slice(-2);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     var cells = document.querySelectorAll(".calendar-cell");
     var scheduleForm = document.getElementById("schedule-form");
     var dateInput = scheduleForm.querySelector("#date-input");
+    var schStartTime = scheduleForm.querySelector("#sch-start");
+    var schEndTime = scheduleForm.querySelector("#sch-end");
 
     cells.forEach(function(cell) {
         cell.addEventListener("click", function() {
             var date = this.getAttribute("data-date");
             dateInput.value = date;
+            schStartTime.value = "08:00";
+            schEndTime.value = "09:00";
             scheduleForm.style.display = "block";
         });
     });
@@ -22,5 +30,39 @@ document.addEventListener("DOMContentLoaded", function() {
         // For now, just hide the form
         scheduleForm.style.display = "none";
     });
+
+    // Add listeners for start and end time
+    schStartTime.addEventListener("input", handleTimeInput);
+    schEndTime.addEventListener("input", handleTimeInput);
+
+    function handleTimeInput(event) {
+        var startTime = schStartTime.value.split(":");
+        var startHour = parseInt(startTime[0]);
+        var startMin = parseInt(startTime[1]);
+
+        var endTime = schEndTime.value.split(":");
+        var endHour = parseInt(endTime[0]);
+        var endMin = parseInt(endTime[1]);
+
+        if (startHour < endHour || (startHour == endHour && startMin <= endMin)) return;
+
+        if (event.target == schStartTime) {
+            // Adjust endTime to startTime + 1 hour
+            endHour = startHour + 1;
+            if (endHour > 23) {
+                endHour = 23;
+                endMin = 59;
+            }
+            schEndTime.value = zeroPad2(endHour) + ":" + zeroPad2(endMin);
+        } else {
+            // Adjust startTime to endTime - 1 hour
+            startHour = endHour - 1;
+            if (startHour < 0) {
+                startHour = 0;
+                startMin = 0;
+            }
+            schStartTime.value = zeroPad2(startHour) + ":" + zeroPad2(startMin);
+        }
+    }
 });
   
